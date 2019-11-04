9973870e0b55b9ce05d6e9d04797111176701bb8
UtkarshAfour <utkarsh.singh@afourtech.com>
2019-11-04 17:32:50 +0530

    checking mui library

--- app/containers/HrForm/index.js
+++ app/containers/HrForm/index.js
@@ -19,109 +19,8 @@ import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
 
-/*
-https://material-ui.com/components/tables/#virtualized-table
-Below import are from this url
-Example : Custom Pagination action
-
-*/
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
 
 
-/*
-  Move this css into seperate file later
-
-*/
-
-const useStyles1 = makeStyles(theme => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-  },
-}));
-
-
-/*
-
-  Pagination logic is below
-
-*/
-function TablePaginationActions(props) {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onChangePage } = props;
-
-  const handleFirstPageButtonClick = event => {
-    onChangePage(event, 0);
-  };
-
-  const handleBackButtonClick = event => {
-    onChangePage(event, page - 1);
-  };
-
-  const handleNextButtonClick = event => {
-    onChangePage(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = event => {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onChangePage: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
-
-/*
-
-  Creating Dummy Data
-
-*/
 
 function createData(empId ,empName ,currentQuarter ,empManager  ,empStatus ) {
   return { empId, empName, currentQuarter ,empManager ,empStatus };
@@ -135,108 +34,16 @@ const rows = [
   createData('AFT0021', 'empName5', 'Q1','empManager5', 'Review Complete'),
 ];
 
-const useStyles2 = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-  },
-  table: {
-    minWidth: 500,
-  },
-  tableWrapper: {
-    overflowX: 'auto',
-  },  
-  paper: {
-    marginTop: theme.spacing(3),
-    width: '100%',
-    overflowX: 'auto',
-    marginBottom: theme.spacing(2),
-  },
-}));
-
-
-
-
 
 export function HrForm() {
   useInjectReducer({ key: 'hrForm', reducer });
   useInjectSaga({ key: 'hrForm', saga });
 
-  /*
-
-    
-
-  */
- const [selected, setSelected] = React.useState([]);
- const classes = useStyles2();
- const [page, setPage] = React.useState(0);
- const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
- const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
- const handleChangePage = (event, newPage) => {
-   setPage(newPage);
- };
-
- const handleChangeRowsPerPage = event => {
-   setRowsPerPage(parseInt(event.target.value, 10));
-   setPage(0);
- };
 
   
   return (
     <div>
-      <Paper className={classes.root}>
-      <div className={classes.tableWrapper}>
-        <Table className={classes.table} aria-label="custom pagination table">
-          <TableHead>
-              <TableRow>
-              <TableCell>Employee ID</TableCell>
-              <TableCell align="right">Employee Name</TableCell>
-              <TableCell align="right">Current Quarter&nbsp;</TableCell>
-              <TableCell align="right">Manager&nbsp;</TableCell>
-              <TableCell align="right">Status&nbsp;</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
-              <TableRow key={row.name}>
-                <TableCell align="right">{row.empId}</TableCell>
-                <TableCell align="right">{row.empName}</TableCell>
-                <TableCell align="right">{row.currentQuarter}</TableCell>
-                <TableCell align="right">{row.empManager}</TableCell>
-                <TableCell align="right">{row.empStatus}</TableCell>
-
-              </TableRow>
-            ))}
-
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                colSpan={3}
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: { 'aria-label': 'rows per page' },
-                  native: true,
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
-    </Paper>
+      
     </div>
   );
 }
