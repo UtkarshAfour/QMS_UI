3c6af8f6bc73126a50fec07d3cab49fa58e95d65
UtkarshAfour <utkarsh.singh@afourtech.com>
2019-11-04 17:49:27 +0530

    Done

--- app/containers/HrForm/index.js
+++ app/containers/HrForm/index.js
@@ -19,20 +19,75 @@ import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
 
+/*
+  import mui -datatables
 
+*/
+
+import MUIDataTable from "mui-datatables";
+
+const columns = [
+  {
+    name : "empId",
+    label : "EmployeeID",
+    option: {
+      filter: false,
+      sort: true
+    }
+  },
+  {
+    name: "empName",
+    label: "Name",
+    options: {
+     filter: false,
+     sort: true,
+    }
+   },
+   {
+    name: "currentQuarter",
+    label: "Current Quarter",
+    options: {
+     filter: false,
+     sort: false,
+    }
+   },
+   {
+    name: "empManager",
+    label: "Manager",
+    options: {
+     filter: true,
+     sort: false,
+    }
+   },
+   {
+    name: "empStatus",
+    label: "Status",
+    options: {
+     filter: true,
+     sort: false,
+    }
+   },
+];
 
 
 function createData(empId ,empName ,currentQuarter ,empManager  ,empStatus ) {
-  return { empId, empName, currentQuarter ,empManager ,empStatus };
+  return { empId: empId, empName: empName, currentQuarter: currentQuarter ,empManager: empManager ,empStatus: empStatus };
 }
 
-const rows = [
+const data = [
   createData('AFT0071', 'empName ', 'Q1', 'empManager', 'Manager Review Pending' ),
   createData('AFT0101', 'empName2', 'Q1', 'empManager2', 'DU Head Review Pending'),
   createData('AFT0331', 'empName3', 'Q1','empManager3', 'HR Review Pending'),
   createData('AFT0041', 'empName4', 'Q1','empManager4', 'Review Complete'),
   createData('AFT0021', 'empName5', 'Q1','empManager5', 'Review Complete'),
 ];
+
+/*
+  select checkbox among various option
+*/
+const options = {
+  filterType: 'checkbox',
+};
 
 
 export function HrForm() {
@@ -43,7 +98,12 @@ export function HrForm() {
   
   return (
     <div>
-      
+      <MUIDataTable
+        title={"Employee List"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
     </div>
   );
 }
